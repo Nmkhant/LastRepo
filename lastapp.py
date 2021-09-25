74cdcd1606acb2f70de715ee73ac18fa34685d73
Nyi Min Khant <85074581+Nmkhant@users.noreply.github.com>
2021-09-25 21:56:21 +0630

    Update lastapp.py

--- lastapp.py
+++ lastapp.py
@@ -50,13 +50,13 @@ elif choice == "About Us":
     
     col1 , col2 , col3 = st.columns(3)
     col1.image('hhm.png', width = 200)
-    col1.write("<p style = 'text-align: left; font-size:110%; color:#1DA1F2'>I am htet htet mon.I am responsible for the backend system of this project.</p>", unsafe_allow_html = True)
+    col1.write("<p style = 'text-align: justify; font-size:110%; color:#1DA1F2'>I am Ma Htet Htet Mon.I am responsible for the backend system of this project.</p>", unsafe_allow_html = True)
     
     col2.image('nmk.png', width = 200)
-    col2.write("<p style = 'text-align: left; font-size:110%; color:#1DA1F2'>I am Nyi Min Khant. I am a student from UTYCC. I made the User Interface of this software.</p>", unsafe_allow_html = True)
+    col2.write("<p style = 'text-align: justify; font-size:110%; color:#1DA1F2'>I am Mg Nyi Min Khant. I am the UI designer of this software.</p>", unsafe_allow_html = True)
     
     col3.image('tyn.png', width = 200)  
-    col3.write("<p style = 'text-align: left; font-size:110%; color:#1DA1F2'></p>", unsafe_allow_html = True)
+    col3.write("<p style = 'text-align: justify; font-size:110%; color:#1DA1F2'>I am Ma Thwet Yin Nyein.I am responsible for data preparation and preprocessing in this project.</p>", unsafe_allow_html = True)
 
 
 elif choice == "Data Visualization":
